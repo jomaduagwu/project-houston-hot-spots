f09c8734ce398dcab3ade9d4e1571d8cfb39961d
Phil-Pfister <salshouse@gmail.com>
2023-04-01 13:20:41 -0500

    added styling to input box and navbar

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,20 @@
+searchTerm = 'cultural';
+
+
 // open trip
 apiKey = '5ae2e3f221c38a28845f05b6fc7cd900dbdfe52e778e3a8d9a518c53'
 // open trip
 apiUrl = 'http://api.opentripmap.com/0.1/ru/places/bbox?lon_min=-95.358421&lat_min=29.749907&lon_max=-95.372809&lat_max=29.859052&kinds=churches&format=geojson&apikey='
-openTrip = 'https://api.opentripmap.com/0.1/en/places/radius?radius=32186&lon=-95.3632&lat=29.7662&kinds=foods&rate=1&limit=50&apikey=5ae2e3f221c38a28845f05b6fc7cd900dbdfe52e778e3a8d9a518c53';
-openTrip = 'https://api.opentripmap.com/0.1/en/places/radius?radius=32186&lon=-95.3632&lat=29.7662&kinds=foods&rate=2&limit=50&apikey=5ae2e3f221c38a28845f05b6fc7cd900dbdfe52e778e3a8d9a518c53';
+openTrip = 'https://api.opentripmap.com/0.1/en/places/radius?radius=32186&lon=-95.3632&lat=29.7662&kinds=' + searchTerm + '&rate=1&limit=50&apikey=' + apiKey;
+
+
 // api - opentrip documentation = https://opentripmap.io/docs
+
+//datepicker
+// $( function() {
+//     $( "#datepicker" ).datepicker();
+//   } );
+  
 
 fetch(openTrip) 
     .then(function (response) {
